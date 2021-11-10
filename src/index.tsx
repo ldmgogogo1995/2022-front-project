066547ca405d855dfce8f09de8032191e9e58458
luodaming <775495877@qq.com>
2021-11-11 07:49:48 +0800

    项目基础搭建

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+
+
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+
+
+const App = () => {
+    return <h2>开始毕业设计！！</h2>
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
